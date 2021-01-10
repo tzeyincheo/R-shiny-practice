56482826654decf41f41c7cbbd3867f5f9b20c9f
tzeyincheo <76771060+tzeyincheo@users.noreply.github.com>
2021-01-10 15:04:59 +0800

    Add files via upload

---
+++ histogram.R
@@ -0,0 +1,63 @@
+####################################
+# Data Professor                   #
+# http://youtube.com/dataprofessor #
+# http://github.com/dataprofessor  #
+####################################
+
+# Modified from https://shiny.rstudio.com/tutorial/written-tutorial/lesson1/
+
+library(shiny)
+data(airquality)
+
+# Define UI for app that draws a histogram ----
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel("Ozone Concentration"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      # Input: Slider for the number of bins ----
+      sliderInput(inputId = "bins",
+                  label = "Number of bins:",
+                  min = 2,
+                  max = 50,
+                  value = 30,
+                  step = 2)
+      
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      # Output: Histogram ----
+      plotOutput(outputId = "distPlot")
+      
+    )
+  )
+)
+
+# Define server logic required to draw a histogram ----
+server <- function(input, output) {
+  
+
+  output$distPlot <- renderPlot({
+    
+    x    <- airquality$Ozone
+    x    <- na.omit(x)
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    hist(x, breaks = bins, col = "#75AADB", border = "white",
+         xlab = "Ozone Conc.",
+         main = "Histogram of Ozone Concentration")
+    
+  })
+  
+}
+
+# Create Shiny app ----
+shinyApp(ui = ui, server = server)
